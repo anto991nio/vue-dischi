684e11c09255d3f7a19f6c0c6d198e798926db69
anto991nio <antonio.91.cardone@gmail.com>
2021-05-13 12:22:24 +0200

    Aggiunge il select e termina l'esercizio

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
 new Vue({
     el: "#app",
     data: {
+        filteredData:[],
         listAlbums: [],
+        genrLista: [],
+        genreToFilter: ""
+
 
     }, mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
-                .then((response) => {
-                this.listAlbums =  response.data.response;
-                })
+            .then((response) => {
+                this.sort(response.data.response)
+                this.listAlbums = response.data.response;
+                this.filteredData=response.data.response;
+                this.generategenreList()
+            })
+          
+
 
 
     },
-
-    methods:{
-        sort(list){
-            list.sort(function(albumA,albumB){
-                const dateA = moment(albumA.year,"YYYY");
+    methods: {
+        sort(listcopy) {
+            listcopy.sort((albumA, albumB) => {
+                const dateA = moment(albumA.year, "YYYY");
                 const dateB = moment(albumB.year, "YYYY");
 
-                if(dateA.isAfter(dateB)){
+                if (dateA.isAfter(dateB)) {
                     return 1;
-                }else if(dateA.isBefore(dateB)){
+                } else if (dateA.isBefore(dateB)) {
                     return -1;
                 }
                 return 0;
             })
-            return list
-        }
-    
-    }
+            return listcopy
+        },
+        generategenreList() {
+            this.listAlbums.forEach((element) => {
+                if (!this.genrLista.includes(element.genre)) {
+                    this.genrLista.push(element.genre)
+                }
+            })
+        },
+        onSelectChange(){
+            if(this.genreToFilter === ""){
+                this.filteredData = this.listAlbums
+                return
 
+            }
+
+            const newFilteredData = this.listAlbums.filter((element)=>{
+                return element.genre === this.genreToFilter
+            })
+
+            this.filteredData = newFilteredData
+        }
+    },
 })
